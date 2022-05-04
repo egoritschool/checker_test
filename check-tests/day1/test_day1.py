7dd66abb8c0ffc3a5603e3e6d5946a9c703b2da2
Egor Fedorov <egor.fedorov@gmail.com>
2022-05-04 19:50:49 +0600

    add code, and tests

---
+++ check-tests/day1/test_day1.py
@@ -0,0 +1,42 @@
+import filecmp
+import os
+
+import pytest
+
+
+def not_changed(path):
+    upstream_root = '/home/universe.dart.spb/efedorov/projects/school-crm/check_python/git-action/sample/upstream-repo'
+    fork_root = '/home/universe.dart.spb/efedorov/projects/school-crm/check_python/git-action/sample/fork'
+    upstream_path = os.path.join(upstream_root, path)
+    fork_path = os.path.join(fork_root, path)
+    if not os.path.exists(fork_path):
+        return True
+    if os.path.isdir(fork_path):
+        dirs_cmp = filecmp.dircmp(upstream_path, fork_path)
+        if dirs_cmp.right_only or dirs_cmp.diff_files or dirs_cmp.funny_files:
+            return False
+    else:
+        return filecmp.cmp(upstream_path, fork_path)
+    return True
+
+
+@pytest.mark.skipif(not_changed('day1/task1.py'), reason='not updated')
+def test_task1():
+    from day1 import script
+    with pytest.raises(ValueError):
+        script.method()
+
+
+@pytest.mark.skipif(not_changed('day1'), reason='not updated')
+def test_task1_other_test():
+    print('Output from the test')
+    assert [1,2,3] == [1,2]
+    pytest.fail('Should think before you type')
+
+
+@pytest.mark.skipif(not_changed('day1'), reason='not updated')
+def test_overall():
+    from day1 import script
+    print('Output from the test')
+    with pytest.raises(ValueError):
+        script.method()
